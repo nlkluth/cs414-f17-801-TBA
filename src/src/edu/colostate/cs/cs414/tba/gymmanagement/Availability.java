e2fd7b82e2c022e6ff766c69989f0021a09efc89
Nathan Kluth <nlkluth@gmail.com>
2017-10-15 22:02:38 -0600

    add the enums since they are only thing i am pretty sure of

---
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/Availability.java
@@ -0,0 +1,9 @@
+package edu.colostate.cs.cs414.tba.gymmanagement;
+
+public enum Availability {
+	FULLTIME,
+	PARTTIME,
+	SEASONAL_FULLTIME,
+	SEASONAL_PARTTIME,
+	UNAVAILABLE
+}
---
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/Membership.java
@@ -0,0 +1,6 @@
+package edu.colostate.cs.cs414.tba.gymmanagement;
+
+public enum Membership {
+	ACTIVE,
+	INACTIVE
+}
