b1c76b4358a6a046521b448d58af7df129dca1f5
Nathan Kluth <nlkluth@gmail.com>
2017-10-20 10:53:26 -0600

    insurance getter/setters

---
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/Insurance.java
@@ -0,0 +1,36 @@
+package edu.colostate.cs.cs414.tba.gymmanagement;
+
+public class Insurance {
+	private String name;
+	private Address address;
+	
+	public Insurance(String name, Address address) {
+		this.name = name;
+		this.address = address;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Address getAddress() {
+		return this.address;
+	}
+	
+	@Override
+	// two workers are equal iff their names match
+	public boolean equals(Object insurance) {
+		return ((Insurance) insurance).getName().equals(name);
+	}
+	
+	@Override
+	public String toString() {
+		return name + ":\n" + address.toString(); 
+	}
+	
+	@Override
+	// used for equality check
+	public int hashCode() {
+		return this.name.hashCode();
+	}
+}
--- src/src/edu/colostate/cs/cs414/tba/gymmanagement/InsuranceTest.java
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/InsuranceTest.java
@@ -4,11 +4,37 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class InsuranceTest {
+	private Insurance insurance;
+	
+	@Before public void setUp() {
+		// create new instance of insurance for tests
+		insurance = new Insurance("Blue Cross", new Address("1234", "2", "Denver", "CO", "80231"));
+	}
+	
 	@Test
-	public void testSetup() throws IOException {
-		assertEquals(true, true);
+	public void testGetName() throws IOException {
+		assertEquals("Blue Cross", insurance.getName());
+	}
+	
+	@Test
+	public void testGetAddress() throws IOException {
+		assertEquals("1234 2, Denver CO, 80231", insurance.getAddress().toString());
+	}
+	
+	@Test
+	public void testToString() throws IOException {
+		assertEquals("Blue Cross:\n1234 2, Denver CO, 80231", insurance.toString());
+	}
+	
+	@Test
+	public void testEquals() throws IOException {
+		Insurance otherInsurance = new Insurance("Blue Cross", new Address("1234", "2", "Denver", "CO", "80231"));
+		Insurance thirdInsurance = new Insurance("Blue Shield", new Address("1234", "2", "Denver", "CO", "80231"));
+		assertEquals(true, insurance.equals(otherInsurance));
+		assertEquals(false, insurance.equals(thirdInsurance));
 	}
 }
