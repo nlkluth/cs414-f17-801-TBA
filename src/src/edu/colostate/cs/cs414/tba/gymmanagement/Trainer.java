1432e4635a563e8af87787c79bd268ac8251d49d
Nathan Kluth <nlkluth@gmail.com>
2017-10-21 15:31:07 -0600

    Trainer tests

--- src/src/edu/colostate/cs/cs414/tba/gymmanagement/Trainer.java
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/Trainer.java
@@ -4,11 +4,13 @@ public class Trainer {
 	private PersonalInformation personalInformation;
 	private Address address;
 	private Insurance insurance;
+	private Availability availability;
 	
 	public Trainer(PersonalInformation personalInformation, Address address, Insurance insurance) {
 		this.personalInformation = personalInformation;
 		this.address = address;
 		this.insurance = insurance;
+		availability = Availability.FULLTIME;
 	}
 	
 	@Override
@@ -16,12 +18,36 @@ public class Trainer {
 		return this.personalInformation.getName() == ((Trainer) other).getPersonalInformation().getName();
 	}
 	
-	private PersonalInformation getPersonalInformation() {
+	public PersonalInformation getPersonalInformation() {
 		return this.personalInformation;
 	}
 
 	@Override
 	public int hashCode() {
 		return this.personalInformation.getName().hashCode();
+	}
+	
+	public void setAvailability(Availability availability) {
+		this.availability = availability;
+	}
+	
+	public Availability getAvailability() {
+		return this.availability;
+	}
+
+	public Address getAddress() {
+		return this.address;
+	}
+
+	public Insurance getInsurance() {
+		return this.insurance;
+	}
+
+	public void update(PersonalInformation personalInformation,
+			Address address, Insurance insurance) {
+		this.address = address;
+		this.personalInformation = personalInformation;
+		this.insurance = insurance;
+		
 	}
 }
--- src/src/edu/colostate/cs/cs414/tba/gymmanagement/TrainerTest.java
+++ src/src/edu/colostate/cs/cs414/tba/gymmanagement/TrainerTest.java
@@ -4,11 +4,56 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TrainerTest {
+	private Trainer trainer;
+	
+	@Before public void setUp() {
+		trainer = new Trainer(
+				new PersonalInformation("Nathan", "Kluth", "555-555-5555", "test@example.com"),
+				new Address("2345", "4", "Denver", "CO", "80231"),
+				new Insurance("Blue Cross", new Address("234", "4", "Denver", "CO", "80231"))
+				);
+	}
+	
 	@Test
-	public void testSetup() throws IOException {
-		assertEquals(true, true);
+	public void testUpdate() {
+		trainer.update(
+				new PersonalInformation("Test", "User", "444-444-4444", "Test@example.com"),
+				new Address("2424", "4", "Denver", "CO", "80122"),
+				new Insurance("eawf aewf", new Address("234", "4", "Denver", "CO", "80231"))
+				);
+		
+		assertEquals("User: Test User\nphone: 444-444-4444\nemail: Test@example.com", trainer.getPersonalInformation().toString());
+		assertEquals("2424 4, Denver CO, 80122", trainer.getAddress().toString());
+		assertEquals("eawf aewf:\n234 4, Denver CO, 80231", trainer.getInsurance().toString());
+	}
+	
+	@Test
+	public void testGetPersonalInformation() {
+		assertEquals("User: Nathan Kluth\nphone: 555-555-5555\nemail: test@example.com", trainer.getPersonalInformation().toString());
+	}
+	
+	@Test
+	public void testGetInsurance() {
+		assertEquals("Blue Cross:\n234 4, Denver CO, 80231", trainer.getInsurance().toString());
+	}
+	
+	@Test
+	public void testGetAddress() {
+		assertEquals("2345 4, Denver CO, 80231", trainer.getAddress().toString());
+	}
+	
+	@Test
+	public void testGetAvailability() {
+		assertEquals(Availability.FULLTIME, trainer.getAvailability());
+	}
+	
+	@Test
+	public void testSetAvailability() {
+		trainer.setAvailability(Availability.PARTTIME);
+		assertEquals(Availability.PARTTIME, trainer.getAvailability());
 	}
 }
