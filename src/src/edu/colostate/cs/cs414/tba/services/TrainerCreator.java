a2d14668cd48e95ad464d670d472825f11b595b3
Nathan Kluth <nlkluth@gmail.com>
2017-11-16 08:40:16 -0700

    move modify over too

--- src/src/edu/colostate/cs/cs414/tba/services/TrainerCreator.java
+++ src/src/edu/colostate/cs/cs414/tba/services/TrainerCreator.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import edu.colostate.cs.cs414.tba.application.GymSystem;
 import edu.colostate.cs.cs414.tba.domain.Address;
 import edu.colostate.cs.cs414.tba.domain.Insurance;
 import edu.colostate.cs.cs414.tba.domain.Manager;
@@ -65,6 +66,64 @@ public class TrainerCreator {
 		Trainer trainer = (Trainer) userFactory.createUser("trainer", username, password, personalInformation, address, insurance);
 		manager.hireTrainer(trainer);
 		System.out.println("\n ***Trainer added to system*** \n");		
+	}
+
+	public void modifyTrainer(Manager manager, GymSystem gymSystem) throws IOException {
+		System.out.println("Enter trainer first name");
+		String name = reader.readLine();
+		System.out.println("Enter trainer last name");
+		String lastName = reader.readLine();
+		
+		for (Trainer trainer : gymSystem.getTrainers()) {
+			if (trainer.getPersonalInformation().getName().equals(name + " " + lastName)) {
+				System.out.println("Trainer found");
+				System.out.println("\nEnter Personal Information");
+				System.out.println("Name:");
+				String newName = reader.readLine();
+				System.out.println("Last Name:");
+				String newLast = reader.readLine();
+				System.out.println("Phone:");
+				String phone = reader.readLine();
+				System.out.println("email:");
+				String email = reader.readLine();
+				PersonalInformation personalInformation = new PersonalInformation(newName, newLast, phone, email);
+				
+				System.out.println("\nEnter Address information");
+				System.out.println("Street");
+				String street = reader.readLine();
+				System.out.println("Street 2");
+				String street2 = reader.readLine();
+				System.out.println("City");
+				String city = reader.readLine();
+				System.out.println("State");
+				String state = reader.readLine();
+				System.out.println("Zip");
+				String zip = reader.readLine();
+				Address address = new Address(street, street2, city, state, zip);
+				
+				System.out.println("\nEnter insurance information");
+				System.out.println("Insurance name");
+				String insuranceName = reader.readLine();
+				System.out.println("Insurance street");
+				String insuranceStreet = reader.readLine();
+				System.out.println("Insurance street 2");
+				String insuranceStreet2 = reader.readLine();
+				System.out.println("Insurance city");
+				String insuranceCity = reader.readLine();
+				System.out.println("Insurance state");
+				String insuranceState = reader.readLine();
+				System.out.println("Insurance zip");
+				String insuranceZip = reader.readLine();
+				Address insuranceAddress = new Address(insuranceStreet, insuranceStreet2, insuranceCity, insuranceState, insuranceZip);
+				Insurance insurance = new Insurance(insuranceName, insuranceAddress);
+				
+				trainer.update(personalInformation, address, insurance);
+				System.out.println("\n *** Trainer updated in the system *** \n");
+				return;
+			}
+		}
+		
+		System.out.println("Error: no trainer found with that name");
 	}
 
 }
