5368273632eac1fd4f7aa72cab345f92a9000115
Nathan Kluth <nlkluth@gmail.com>
2017-11-16 08:43:10 -0700

    move modify

--- src/src/edu/colostate/cs/cs414/tba/services/CustomerCreator.java
+++ src/src/edu/colostate/cs/cs414/tba/services/CustomerCreator.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import edu.colostate.cs.cs414.tba.application.GymSystem;
 import edu.colostate.cs.cs414.tba.domain.Address;
 import edu.colostate.cs.cs414.tba.domain.Customer;
 import edu.colostate.cs.cs414.tba.domain.Insurance;
@@ -59,6 +60,64 @@ public class CustomerCreator {
 		Customer customer = (Customer) userFactory.createUser("customer", null, null, personalInformation, address, insurance);
 		manager.registerCustomer(customer);
 		System.out.println("\n ***Customer added to system*** \n");
+	}
+
+	public void modifyCustomer(Manager manager, GymSystem gymSystem) throws IOException {
+		System.out.println("Enter customer first name");
+		String name = reader.readLine();
+		System.out.println("Enter customer last name");
+		String lastName = reader.readLine();
+		
+		for (Customer customer : gymSystem.getCustomers()) {
+			if (customer.getPersonalInformation().getName().equals(name + " " + lastName)) {
+				System.out.println("Customer found");
+				System.out.println("\nEnter Personal Information");
+				System.out.println("Name:");
+				String newName = reader.readLine();
+				System.out.println("Last Name:");
+				String newLast = reader.readLine();
+				System.out.println("Phone:");
+				String phone = reader.readLine();
+				System.out.println("email:");
+				String email = reader.readLine();
+				PersonalInformation personalInformation = new PersonalInformation(newName, newLast, phone, email);
+				
+				System.out.println("\nEnter Address information");
+				System.out.println("Street");
+				String street = reader.readLine();
+				System.out.println("Street 2");
+				String street2 = reader.readLine();
+				System.out.println("City");
+				String city = reader.readLine();
+				System.out.println("State");
+				String state = reader.readLine();
+				System.out.println("Zip");
+				String zip = reader.readLine();
+				Address address = new Address(street, street2, city, state, zip);
+				
+				System.out.println("\nEnter insurance information");
+				System.out.println("Insurance name");
+				String insuranceName = reader.readLine();
+				System.out.println("Insurance street");
+				String insuranceStreet = reader.readLine();
+				System.out.println("Insurance street 2");
+				String insuranceStreet2 = reader.readLine();
+				System.out.println("Insurance city");
+				String insuranceCity = reader.readLine();
+				System.out.println("Insurance state");
+				String insuranceState = reader.readLine();
+				System.out.println("Insurance zip");
+				String insuranceZip = reader.readLine();
+				Address insuranceAddress = new Address(insuranceStreet, insuranceStreet2, insuranceCity, insuranceState, insuranceZip);
+				Insurance insurance = new Insurance(insuranceName, insuranceAddress);
+				
+				customer.update(personalInformation, address, insurance);
+				System.out.println("\n *** Customer updated in the system *** \n");
+				return;
+			}
+		}
+		
+		System.out.println("Error: no customer found with that name");
 	}
 
 }
