517bbf01d5205dd4a668b6f30a963fdcae899591
Nathan Kluth <nlkluth@gmail.com>
2017-11-16 08:47:56 -0700

    workoutcreator

--- src/src/edu/colostate/cs/cs414/tba/services/CLIController.java
+++ src/src/edu/colostate/cs/cs414/tba/services/CLIController.java
@@ -6,17 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import edu.colostate.cs.cs414.tba.application.GymSystem;
-import edu.colostate.cs.cs414.tba.domain.Address;
 import edu.colostate.cs.cs414.tba.domain.Customer;
 import edu.colostate.cs.cs414.tba.domain.Equipment;
 import edu.colostate.cs.cs414.tba.domain.Exercise;
-import edu.colostate.cs.cs414.tba.domain.Insurance;
 import edu.colostate.cs.cs414.tba.domain.Manager;
-import edu.colostate.cs.cs414.tba.domain.PersonalInformation;
 import edu.colostate.cs.cs414.tba.domain.Trainer;
 import edu.colostate.cs.cs414.tba.domain.WorkoutRoutine;
 import edu.colostate.cs.cs414.tba.gymmanagement.ExerciseBuilder;
-import edu.colostate.cs.cs414.tba.gymmanagement.UserFactory;
 
 /**
  * Simple Command line interface
@@ -30,6 +26,7 @@ public class CLIController {
 	private ExerciseBuilder exerciseBuilder;
 	private static CLIController uniqueInstance;
 	private TrainerCreator trainerCreator;
+	private WorkoutCreator workoutCreator;
 	
 	private CLIController() {}
 	
@@ -192,43 +189,7 @@ public class CLIController {
 			return;
 		}
 		
-		Customer customer = null;
-		WorkoutRoutine workout = null;
-		
-		while (user == null) {
-			System.out.println("What is the customer firstName?");
-			String name = reader.readLine();
-			System.out.println("What is the customer lastName?");
-			String lastName = reader.readLine();
-			
-			for (Customer foundCustomer : gymSystem.getCustomers()) {
-				if (foundCustomer.getPersonalInformation().getName().toLowerCase().equals(name + " " + lastName)) {
-					customer = foundCustomer;
-					break;
-				}
-			}
-			
-			System.out.println("User not found");
-		}
-		
-		while (workout == null) {
-			System.out.println("What workout would you like to assign?");
-			String workoutName = reader.readLine();
-			
-			for (WorkoutRoutine foundWorkout : gymSystem.getWorkoutRoutines()) {
-				if (foundWorkout.getName().toString().equals(workoutName)) {
-					workout = foundWorkout;
-					break;
-				}
-			}
-			
-			System.out.println("WorkoutRoutine not found");
-		}
-		
-		workout.assignCustomer(customer);
-		customer.addWorkoutRoutine(workout);
-		
-		System.out.println("\n *** Assigned customer to workout routine *** \n");
+		workoutCreator.assignWorkout((Trainer) this.user, this.gymSystem);
 	}
 
 	private void searchCustomers() {
@@ -345,73 +306,7 @@ public class CLIController {
 			return;
 		}
 		
-		System.out.println("Enter name of workout:");
-		String name = reader.readLine();
-		
-		WorkoutRoutine workoutRoutine = new WorkoutRoutine(name);
-		exerciseBuilder = new ExerciseBuilder();
-		
-		boolean addingExercises = true;
-		while (addingExercises) {
-			System.out.println("Enter exercise name: ");
-			String exerciseName = reader.readLine();
-			exerciseBuilder.setName(exerciseName);
-			System.out.println("Enter duration of workout: ");
-			String duration = reader.readLine();
-			exerciseBuilder.setDuration(duration);
-			System.out.println("Enter a number of sets:");
-			
-			try {
-				String setsString = reader.readLine();
-				int sets = Integer.parseInt(setsString);
-				exerciseBuilder.setSets(sets);
-			} catch (Error e) {
-				System.out.println("sets must be a number");
-			}
-			
-			try {
-				System.out.println("Enter a number of reps");
-				String repsString = reader.readLine();
-				int reps = Integer.parseInt(repsString);
-				exerciseBuilder.setReps(reps);
-			} catch (Error e) {
-				System.out.println("reps must be a number");
-			}			
-			
-			System.out.println("Does this exercise need equipment? (y/n)");
-			String needEquipment = reader.readLine();
-			
-			while (needEquipment.toLowerCase().equals("y")) {
-				Equipment found = null;
-				System.out.println("Which equipment does it need?");
-				String equipmentName = reader.readLine();				
-				
-				for (Equipment equipment : gymSystem.getEquipment()) {
-					if (equipmentName.toLowerCase().equals(equipment.getName().toLowerCase())) {
-						found = equipment;
-					}
-				}
-					
-				if (found == null) {
-					System.out.println("Did not find equipment");
-				} else {
-					exerciseBuilder.addEquipment(found);					
-					needEquipment = "n";
-				}
-			}
-			
-			System.out.println("Are there more exercises? (y/n)");
-			String choice = reader.readLine();
-			
-			if (choice.toLowerCase().equals("n")) {
-				addingExercises = false;				
-			}
-		}
-		
-		Exercise exercise = exerciseBuilder.createExercise();
-		workoutRoutine.addExercise(exercise);
-		gymSystem.addWorkoutRoutine(workoutRoutine);
-		System.out.print("\n ***Workout routine added to system*** \n");
+		workoutCreator.createWorkout((Trainer) this.user, this.gymSystem);
 	}
 
 	private void modifyCustomer() throws IOException {
---
+++ src/src/edu/colostate/cs/cs414/tba/services/WorkoutCreator.java
@@ -0,0 +1,129 @@
+package edu.colostate.cs.cs414.tba.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import edu.colostate.cs.cs414.tba.application.GymSystem;
+import edu.colostate.cs.cs414.tba.domain.Customer;
+import edu.colostate.cs.cs414.tba.domain.Equipment;
+import edu.colostate.cs.cs414.tba.domain.Exercise;
+import edu.colostate.cs.cs414.tba.domain.Trainer;
+import edu.colostate.cs.cs414.tba.domain.WorkoutRoutine;
+import edu.colostate.cs.cs414.tba.gymmanagement.ExerciseBuilder;
+
+public class WorkoutCreator {
+	private ExerciseBuilder exerciseBuilder;
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));	
+	
+	public void createWorkout(Trainer user, GymSystem gymSystem) throws IOException {
+		System.out.println("Enter name of workout:");
+		String name = reader.readLine();
+		
+		WorkoutRoutine workoutRoutine = new WorkoutRoutine(name);
+		exerciseBuilder = new ExerciseBuilder();
+		
+		boolean addingExercises = true;
+		while (addingExercises) {
+			System.out.println("Enter exercise name: ");
+			String exerciseName = reader.readLine();
+			exerciseBuilder.setName(exerciseName);
+			System.out.println("Enter duration of workout: ");
+			String duration = reader.readLine();
+			exerciseBuilder.setDuration(duration);
+			System.out.println("Enter a number of sets:");
+			
+			try {
+				String setsString = reader.readLine();
+				int sets = Integer.parseInt(setsString);
+				exerciseBuilder.setSets(sets);
+			} catch (Error e) {
+				System.out.println("sets must be a number");
+			}
+			
+			try {
+				System.out.println("Enter a number of reps");
+				String repsString = reader.readLine();
+				int reps = Integer.parseInt(repsString);
+				exerciseBuilder.setReps(reps);
+			} catch (Error e) {
+				System.out.println("reps must be a number");
+			}			
+			
+			System.out.println("Does this exercise need equipment? (y/n)");
+			String needEquipment = reader.readLine();
+			
+			while (needEquipment.toLowerCase().equals("y")) {
+				Equipment found = null;
+				System.out.println("Which equipment does it need?");
+				String equipmentName = reader.readLine();				
+				
+				for (Equipment equipment : gymSystem.getEquipment()) {
+					if (equipmentName.toLowerCase().equals(equipment.getName().toLowerCase())) {
+						found = equipment;
+					}
+				}
+					
+				if (found == null) {
+					System.out.println("Did not find equipment");
+				} else {
+					exerciseBuilder.addEquipment(found);					
+					needEquipment = "n";
+				}
+			}
+			
+			System.out.println("Are there more exercises? (y/n)");
+			String choice = reader.readLine();
+			
+			if (choice.toLowerCase().equals("n")) {
+				addingExercises = false;				
+			}
+		}
+		
+		Exercise exercise = exerciseBuilder.createExercise();
+		workoutRoutine.addExercise(exercise);
+		gymSystem.addWorkoutRoutine(workoutRoutine);
+		System.out.print("\n ***Workout routine added to system*** \n");
+	}
+
+	public void assignWorkout(Trainer user, GymSystem gymSystem) throws IOException {
+		Customer customer = null;
+		WorkoutRoutine workout = null;
+		
+		while (user == null) {
+			System.out.println("What is the customer firstName?");
+			String name = reader.readLine();
+			System.out.println("What is the customer lastName?");
+			String lastName = reader.readLine();
+			
+			for (Customer foundCustomer : gymSystem.getCustomers()) {
+				if (foundCustomer.getPersonalInformation().getName().toLowerCase().equals(name + " " + lastName)) {
+					customer = foundCustomer;
+					break;
+				}
+			}
+			
+			System.out.println("User not found");
+		}
+		
+		while (workout == null) {
+			System.out.println("What workout would you like to assign?");
+			String workoutName = reader.readLine();
+			
+			for (WorkoutRoutine foundWorkout : gymSystem.getWorkoutRoutines()) {
+				if (foundWorkout.getName().toString().equals(workoutName)) {
+					workout = foundWorkout;
+					break;
+				}
+			}
+			
+			System.out.println("WorkoutRoutine not found");
+		}
+		
+		workout.assignCustomer(customer);
+		customer.addWorkoutRoutine(workout);
+		
+		System.out.println("\n *** Assigned customer to workout routine *** \n");
+	}
+
+}
