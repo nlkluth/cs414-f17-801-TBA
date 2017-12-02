8d63cc638ed12042d41f90819dcbe7496cc5a869
Nathan Kluth <nlkluth@gmail.com>
2017-12-02 10:19:34 -0700

    trying out abstract class, slimming down contructor as tests were showing it was too much when creating

--- src/src/edu/colostate/cs/cs414/tba/application/User.java
+++ src/src/edu/colostate/cs/cs414/tba/application/User.java
@@ -1,12 +1,82 @@
 package edu.colostate.cs.cs414.tba.application;
 
+import java.rmi.server.UID;
+
 import edu.colostate.cs.cs414.tba.domain.Address;
 import edu.colostate.cs.cs414.tba.domain.Insurance;
 import edu.colostate.cs.cs414.tba.domain.PersonalInformation;
 
-public interface User {
-	public Insurance getInsurance();
-	public Address getAddress();
-	public void update(PersonalInformation personalInformation, Address address, Insurance insurance);
-	public PersonalInformation getPersonalInformation();
+/*
+ * extended by Trainer, Manager, Customer 
+ * @param {username} username for user
+ * @param {password} password for logging in
+ * set {PersonalInformation} user personal info
+ * set {Address} user address
+ * set {Insurance} user insurance
+ *
+ */
+public abstract class User {
+	private PersonalInformation personalInformation;
+	private UID id;
+	private Address address;
+	private Insurance insurance;
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+		this.id = new UID();
+	}
+	
+	public void setPersonalInformation(PersonalInformation personalInformation) {
+		this.personalInformation = personalInformation;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+
+	public PersonalInformation getPersonalInformation() {
+		return this.personalInformation;
+	}
+	
+	public UID getId() {
+		return this.id;
+	}
+	
+	public Address getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	public Insurance getInsurance() {
+		return this.insurance;
+	}
+	
+	public void setInsurance(Insurance insurance) {
+		this.insurance = insurance;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if ((other == null) || (this == null)) {
+	        return false;
+	    }
+		
+		return this.username.equals(((User) other).getUsername());
+	}
+
+	@Override
+	public int hashCode() {
+		return this.username.hashCode();
+	}
+	
 }
--- src/src/edu/colostate/cs/cs414/tba/domain/Trainer.java
+++ src/src/edu/colostate/cs/cs414/tba/domain/Trainer.java
@@ -1,91 +1,27 @@
 package edu.colostate.cs.cs414.tba.domain;
 
-import java.rmi.server.UID;
-
 import edu.colostate.cs.cs414.tba.application.Availability;
 import edu.colostate.cs.cs414.tba.application.User;
 
 /**
  * Created by manager, can manage customers
  * Can assign a customer to a routine and create workouts
- * @param {username} username for trainer
- * @param {password} password for logging in
- * @param {PersonalInformation} trainer's personal info
- * @param {Address} Trainer's address
- * @param {Insurance} Trainer's insurance
- *
+ * Has availability
  */
-public class Trainer implements User {
-	private PersonalInformation personalInformation;
-	private Address address;
-	private Insurance insurance;
+public class Trainer extends User {
 	private Availability availability;
-	private String username;
-	private String password;
-	private UID id;
 	
-	public Trainer(String username, String password, PersonalInformation personalInformation, Address address, Insurance insurance) {
-		this.personalInformation = personalInformation;
-		this.address = address;
-		this.insurance = insurance;
-		this.username = username;
-		this.password = password;
-		this.id = new UID();
-		availability = Availability.FULLTIME;
+	public Trainer(String username, String password) {
+		super(username, password);	
+		this.availability = Availability.FULLTIME;
 	}
 	
-	@Override
-	public boolean equals(Object other) {
-		if ((other == null) || (this == null)) {
-	        return false;
-	    }
-		
-		return this.personalInformation.getName().equals(((Trainer) other).getPersonalInformation().getName());
-	}
-	
-	public PersonalInformation getPersonalInformation() {
-		return this.personalInformation;
-	}
-
-	@Override
-	public int hashCode() {
-		return this.personalInformation.getName().hashCode();
-	}
-	
-	// A trainer has availability, defaults to FULLTIME
 	public void setAvailability(Availability availability) {
+		// A trainer has availability, defaults to FULLTIME
 		this.availability = availability;
 	}
 	
 	public Availability getAvailability() {
 		return this.availability;
-	}
-
-	public Address getAddress() {
-		return this.address;
-	}
-
-	public Insurance getInsurance() {
-		return this.insurance;
-	}
-
-	public void update(PersonalInformation personalInformation,
-			Address address, Insurance insurance) {
-		this.address = address;
-		this.personalInformation = personalInformation;
-		this.insurance = insurance;
-		
-	}
-
-	public String getUsername() {
-		return this.username;
-	}
-	
-	public String getPassword() {
-		return this.password;
-	}
-
-	public UID getId() {
-		return this.id;
 	}
 }
